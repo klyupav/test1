f61828f4f836a539e77b3b4a88df365d2f4a728c
Pavel Klyuchnikov <pasha.klyuchnikov@gmail.com>
2022-12-15 16:47:09 +0500

    encodeData implementation

--- src/ArrayUtil.php
+++ src/ArrayUtil.php
@@ -23,6 +23,30 @@ class ArrayUtil implements ArrayUtilInterface
 
     public function encodeData(array $data): array
     {
-        return [];
+        foreach ($data as $key => $value) {
+            if (is_array($value)) {
+                $this->encodeArray($value, [$key]);
+            } else {
+                $this->result[$key] = $value;
+            }
+        }
+
+        return $this->result;
+    }
+
+    protected function encodeArray(array $data, array $keys): void
+    {
+        foreach ($data as $key => $value) {
+            $tmp = $keys;
+            $tmp[] = $key;
+            $k = implode('.', $tmp);
+            $keys[$k] = $key;
+            if (is_array($value)) {
+                $this->encodeArray($value, $keys);
+            } else {
+                $this->result[$k] = $value;
+            }
+            unset($keys[$k]);
+        }
     }
 }
