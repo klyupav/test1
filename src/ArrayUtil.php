e16a3baa93a578760d438f4f2f327ebbf65ef9c5
Pavel Klyuchnikov <pasha.klyuchnikov@gmail.com>
2022-12-15 16:46:33 +0500

    decodeData implementation

--- src/ArrayUtil.php
+++ src/ArrayUtil.php
@@ -4,9 +4,21 @@ namespace App;
 
 class ArrayUtil implements ArrayUtilInterface
 {
+    protected $result = [];
+
     public function decodeData(array $data): array
     {
-        return [];
+        $result = [];
+        foreach ($data as $key => $value) {
+            $current = &$result;
+            $keys = explode('.', $key);
+            foreach ($keys as $k) {
+                $current = &$current[$k];
+            }
+            $current = $value;
+        }
+
+        return $result;
     }
 
     public function encodeData(array $data): array
